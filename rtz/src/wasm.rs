88ab963c1c4f30f29e2aa02df397d429c02905da
Aaron Roney <twitchax@gmail.com>
2023-07-27 16:04:55 -0700

    Add `osm` tz to WASM exports.

--- rtz/src/wasm.rs
+++ rtz/src/wasm.rs
@@ -19,3 +19,13 @@ pub fn get_timezone_ned(lng: f32, lat: f32) -> JsValue {
         None => JsValue::NULL,
     }
 }
+
+/// Get the time zone for the given `(lng,lat)`.
+#[cfg(feature = "tz-osm")]
+#[wasm_bindgen(js_name = getTimezoneOsm)]
+pub fn get_timezone_osm(lng: f32, lat: f32) -> JsValue {
+    match crate::get_timezone_osm(lng, lat) {
+        Some(tz) => JsValue::from_str(&serde_json::to_string(&tz).unwrap()),
+        None => JsValue::NULL,
+    }
+}
